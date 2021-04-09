f37b859dd6ac55bc5c21b23a9d4c4b1e3507e9de
Paolo Zaccagnini <paolo@pzac.net>
2021-04-09 09:33:25 +0200

    Add pry gem

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,7 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "redgraph"
 
 require "minitest/autorun"
+require "pry"
 
 unless $REDIS_URL = ENV['TEST_REDIS_URL']
   puts "To run the tests you need to define the TEST_REDIS_URL environment variable"
