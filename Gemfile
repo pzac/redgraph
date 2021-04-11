7daf6d855123ab05da7256202218a1b22bac7f94
Paolo Zaccagnini <paolo@pzac.net>
2021-04-11 14:13:20 +0200

    Add test coverage tool

--- Gemfile
+++ Gemfile
@@ -9,3 +9,5 @@ gem "rake", "~> 13.0"
 
 gem "minitest", "~> 5.0"
 gem "pry", "~> 0.14.0"
+gem "simplecov", "~> 0.21.2"
+
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
+require 'simplecov'
+
+SimpleCov.start do
+  enable_coverage :branch
+  add_filter %r{^/test/}
+end
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "redgraph"
