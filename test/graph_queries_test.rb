ef7d76f6e5d9507dfb7d79eb8e2e8f16b43c44e7
Paolo Zaccagnini <paolo@pzac.net>
2021-04-11 09:52:00 +0200

    Clean up tests

--- test/graph_queries_test.rb
+++ test/graph_queries_test.rb
@@ -5,11 +5,9 @@ require "test_helper"
 class GraphQueriesTest < Minitest::Test
   def setup
     @graph = Redgraph::Graph.new("movies", url: $REDIS_URL)
-    @actor = Redgraph::Node.new(label: 'actor', properties: {name: "Al Pacino"})
-    @graph.add_node(@actor)
 
-    @other_actor = Redgraph::Node.new(label: 'actor', properties: {name: "John Travolta"})
-    refute_nil(@actor.id)
+    @al = quick_add_node(label: 'actor', properties: {name: "Al Pacino"})
+    @john = quick_add_node(label: 'actor', properties: {name: "John Travolta"})
   end
 
   def teardown
@@ -17,42 +15,45 @@ class GraphQueriesTest < Minitest::Test
   end
 
   def test_find_node_by_id
-    node = @graph.find_node_by_id(@actor.id)
+    node = @graph.find_node_by_id(@al.id)
 
     refute_nil(node)
     assert_equal("actor", node.label)
     assert_equal("Al Pacino", node.properties["name"])
-    assert_equal(@actor.id, node.id)
-  end
-
-  def test_get_all_nodes
-    @graph.add_node(@other_actor)
-
-    actors = @graph.nodes
-
-    assert_equal(2, actors.size)
-    assert_includes(actors, @actor)
-    assert_includes(actors, @other_actor)
-  end
-
-  def test_get_all_nodes_by_label
-    @graph.add_node(@other_actor)
-    film = Redgraph::Node.new(label: 'film', properties: {name: "Scarface"})
-    @graph.add_node(film)
-
-    actors = @graph.nodes(label: 'actor')
-    assert_equal(2, actors.size)
-    assert_includes(actors, @actor)
-    assert_includes(actors, @other_actor)
-
-    films = @graph.nodes(label: 'film')
-    assert_equal(1, films.size)
-    assert_includes(films, film)
+    assert_equal(@al.id, node.id)
   end
 
   def test_find_node_by_wrong_id
     node = @graph.find_node_by_id("-1")
 
     assert_nil(node)
+  end
+
+  def test_find_all_nodes
+    actors = @graph.nodes
+
+    assert_equal(2, actors.size)
+    assert_includes(actors, @al)
+    assert_includes(actors, @john)
+  end
+
+  def test_find_all_nodes_by_label
+    film = quick_add_node(label: 'film', properties: {name: "Scarface"})
+
+    actors = @graph.nodes(label: 'actor')
+    assert_equal(2, actors.size)
+    assert_includes(actors, @al)
+    assert_includes(actors, @john)
+
+    films = @graph.nodes(label: 'film')
+    assert_equal(1, films.size)
+    assert_includes(films, film)
+  end
+
+
+  private
+
+  def quick_add_node(label:, properties:)
+    @graph.add_node(Redgraph::Node.new(label: label, properties: properties))
   end
 end
